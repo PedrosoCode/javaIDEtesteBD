0e91469c33762da3067db573ca8f872ff2b5e7ae
Gabriel <Gabrielpedrofc@gmail.com>
2024-03-06 22:18:54 -0300

    conexão funcional com MySQL WOrkbench

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -9,6 +9,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) {
@@ -25,16 +30,14 @@ public class HelloApplication extends Application {
         // Criar botão
         Button submitButton = new Button("Submit");
 
-        // Definir a ação do botão
+        // Definir ação do botão
         submitButton.setOnAction(event -> {
             String name = nameField.getText();
             String lastName = lastNameField.getText();
             String email = emailField.getText();
 
-            // Aqui você pode fazer o que quiser com os dados do formulário
-            System.out.println("Name: " + name);
-            System.out.println("Last Name: " + lastName);
-            System.out.println("Email: " + email);
+            // Inserir dados no banco de dados
+            insertData(name, lastName, email);
         });
 
         // Criar um layout de grade para organizar os elementos
@@ -57,6 +60,25 @@ public class HelloApplication extends Application {
         stage.setTitle("Form");
         stage.setScene(scene);
         stage.show();
+    }
+
+    // Método para inserir dados no banco de dados
+    private void insertData(String name, String lastName, String email) {
+        String url = "jdbc:mysql://localhost:3306/java";
+        String user = "root";
+        String password = "@Inspiron1";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            String sql = "INSERT INTO tb_teste (nome, sobrenome, email) VALUES (?, ?, ?)";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setString(3, email);
+            statement.executeUpdate();
+            System.out.println("Dados inseridos com sucesso!");
+        } catch (SQLException e) {
+            System.out.println("Erro ao inserir dados no banco de dados: " + e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,9 @@ module com.example.demo1 {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
 
+    requires java.sql;
+
     opens com.example.demo1 to javafx.fxml;
     exports com.example.demo1;
 }
+
